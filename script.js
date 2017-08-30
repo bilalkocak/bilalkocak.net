835aaeb10d24773718b8d54b1d65286fa84531cb
Bilal Koçak <kocakbilal04@gmail.com>
2017-08-30 13:10:56 +0300

    Js eklendi.

---
+++ script.js
@@ -0,0 +1,66 @@
+var imgGit=document.getElementById('git');
+var imgTwit=document.getElementById('twit');
+var imgFace=document.getElementById('face');
+var imgLink=document.getElementById('link');
+var imgInsta=document.getElementById('insta');
+var imgPlus=document.getElementById('plus');
+var imgMail=document.getElementById('mail');
+
+
+imgGit.onmouseover=function(){
+  this.src="images/github.png";
+}
+
+imgGit.onmouseout=function(){
+  this.src="images/agithub.png";
+}
+
+
+imgTwit.onmouseover=function(){
+  this.src="images/twitter.png";
+}
+
+imgTwit.onmouseout=function(){
+  this.src="images/atwitter.png";
+}
+
+imgFace.onmouseover=function(){
+  this.src="images/facebook.png";
+}
+
+imgFace.onmouseout=function(){
+  this.src="images/afacebook.png";
+}
+
+imgInsta.onmouseover=function(){
+  this.src="images/instagram.png";
+}
+imgInsta.onmouseout=function(){
+  this.src="images/ainstagram.png";
+}
+
+imgLink.onmouseover=function(){
+  this.src="images/linkedin.png";
+}
+
+imgLink.onmouseout=function(){
+  this.src="images/alinkedin.png";
+}
+
+
+imgPlus.onmouseover=function(){
+  this.src="images/googleplus.png";
+}
+
+imgPlus.onmouseout=function(){
+  this.src="images/agoogleplus.png";
+}
+
+
+imgMail.onmouseover=function(){
+  this.src="images/mail.png";
+}
+
+imgMail.onmouseout=function(){
+  this.src="images/amail.png";
+}
